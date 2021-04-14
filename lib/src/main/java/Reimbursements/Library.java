6099512680c3b8614d4a3f3538bb89d30bba8755
Drake Taylor <drake.taylor@revature.net>
2021-04-14 11:44:34 -0500

    Add functionality to Library class

--- lib/src/main/java/Reimbursements/Library.java
+++ lib/src/main/java/Reimbursements/Library.java
@@ -3,15 +3,17 @@ package Reimbursements;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/library")
 public class Library extends HttpServlet {
     private static final long serialVersionUID = 1;
 
-    // @Override
-    // protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
-    // }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Library");
+    }
 }
