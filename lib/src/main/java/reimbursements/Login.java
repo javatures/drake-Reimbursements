3bf8dfe5963fc92b3efd66cda2d7de3224f337af
Drake Taylor <drake.taylor@revature.net>
2021-04-16 14:41:11 -0500

    Add basic login functionality

--- lib/src/main/java/reimbursements/Login.java
+++ lib/src/main/java/reimbursements/Login.java
@@ -8,12 +8,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/library")
-public class Library extends HttpServlet {
+@WebServlet("/login")
+public class Login extends HttpServlet {
     private static final long serialVersionUID = 1;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Library");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String user = req.getParameter("user");
+        String pass = req.getParameter("pass");
+        resp.getWriter().println(user + " " + pass);
     }
 }
